bd63156ea302dd8680cb3b082e2c1c6af0a5f50a
Alex Senties <alexsentiesg@gmail.com>
2025-08-19 14:01:43 -0500

    Preliminary ROS2 Environment and project header/cpp files setup

---
+++ build/flight_stabilization/CMakeFiles/flight_stabilization_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flight_stabilization_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flight_stabilization_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/flight_stabilization/include/flight_stabilization/control/control_actuators.hpp
@@ -0,0 +1,2 @@
+// supposed to control control surface actuators (e.g. PWM)
+// parse an input vector and translate into a surface movement
